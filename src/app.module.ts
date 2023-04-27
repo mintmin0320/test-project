e919047ea72827c165d11ff39620ed5f8c5ea0c0
mintmin <mintmin0320@gmail.com>
2023-04-27 22:19:11 +0900

    Connected to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+
 import { UsersModule } from './users/users.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI), // forRoot : 특정 설정을 모듈에 적용하고 해당 모듈을 여러 곳에서 재사용하는 경우
     UsersModule,
   ],
   controllers: [],
@@ -14,7 +18,9 @@ import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 })
 
 export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
   configure(consumer: MiddlewareConsumer): any {
     consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev); // 배포 시 몽구스 쿼리가 필요없음 false로 변경되도록
   }
 }
